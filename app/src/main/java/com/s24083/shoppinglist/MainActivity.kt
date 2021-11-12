7b47d1a2fe6d041c4392fe296f32d7afc378b098
Damian Ubowski <damian.ubowski@jmr.pl>
2021-11-12 11:18:48 +0100

    Routes between activities

--- app/src/main/java/com/s24083/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/s24083/shoppinglist/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.s24083.shoppinglist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToList(view: View){
+        var intent = Intent(this, ProductListActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun goToSettings(view: View){
+        val intent = Intent(this, OptionsActivity::class.java)
+        startActivity(intent)
     }
 }
