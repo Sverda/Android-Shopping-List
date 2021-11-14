a5996b15987da38411a8a10f0f907e17472c3e9b
Damian Ubowski <damian.ubowski@jmr.pl>
2021-11-14 23:12:58 +0100

    Save settings in SharedPreferences

--- app/src/main/java/com/s24083/shoppinglist/Settings/OptionsActivity.kt
+++ app/src/main/java/com/s24083/shoppinglist/Settings/OptionsActivity.kt
@@ -1,12 +1,41 @@
 package com.s24083.shoppinglist.Settings
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
 import com.s24083.shoppinglist.R
 
 class OptionsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+
+        val sp = this.getPreferences(MODE_PRIVATE)
+
+        val detailsKey = resources.getString(R.string.settingsShowDetails)
+        val detailsValue = sp.getBoolean(detailsKey, false)
+        findViewById<Switch>(R.id.show_details).isChecked = detailsValue
+
+        val sizeKey = resources.getString(R.string.settingsTitleFontSize)
+        val sizeValue = sp.getFloat(sizeKey, 1.0f)
+        findViewById<Slider>(R.id.font_size).value = sizeValue
+    }
+
+    fun saveSettings(view: View){
+        val sp = this.getPreferences(MODE_PRIVATE)
+
+        val detailsKey = resources.getString(R.string.settingsShowDetails)
+        val detailsValue = findViewById<Switch>(R.id.show_details).isChecked
+        val editor = sp.edit()
+        editor.putBoolean(detailsKey, detailsValue)
+
+        val sizeKey = resources.getString(R.string.settingsTitleFontSize)
+        val sizeValue = findViewById<Slider>(R.id.font_size).value
+        editor.putFloat(sizeKey, sizeValue)
+
+        editor.apply()
     }
 }
