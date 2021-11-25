2d7d7cc5dd3373cebb0c99c4e3546b7f8c889860
Damian Ubowski <damian.ubowski.0@gmail.com>
2021-11-25 16:51:54 +0100

    Send broadcast intent after creation of a item

--- app/src/main/java/com/s24083/shoppinglist/addShoppingItem/AddShoppingItemActivity.kt
+++ app/src/main/java/com/s24083/shoppinglist/addShoppingItem/AddShoppingItemActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.viewModels
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
@@ -58,6 +57,7 @@ class AddShoppingItemActivity : AppCompatActivity() {
             resultIntent.putExtra("amount", amount)
             if (currentShoppingItemId == null){
                 resultIntent.putExtra("isBought", false)
+                broadcastItemCreation(name)
             }
             else {
                 val currentItem = viewModel.getItemForId(currentShoppingItemId!!)
@@ -66,5 +66,13 @@ class AddShoppingItemActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK, resultIntent)
         }
         finish()
+    }
+
+    private fun broadcastItemCreation(name: String){
+        Intent().also { intent ->
+            intent.setAction("com.s24083.shoppingItem.ITEM_ADDED")
+            intent.putExtra("name", name)
+            sendBroadcast(intent)
+        }
     }
 }
